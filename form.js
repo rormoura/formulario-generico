e416d330c0eba532a31873cf7d2f7988767bac43
Rodrigo Moura <rrm2@cin.ufpe.br>
2023-06-13 23:26:41 -0300

    Sending all files

---
+++ form.js
@@ -0,0 +1,305 @@
+/*
+https://www.youtube.com/watch?v=ikR9DsGMUMc&t=1177s&ab_channel=SteveGriffith-Prof3ssorSt3v3
+
+https://developer.mozilla.org/en-US/docs/Learn/Forms
+
+https://youtu.be/3bGQ7s0mnHY
+
+a alteração das cidades conforme o estado selecionado demora um pouquinho mas da pra perceber o delay
+
+usando localstorage
+
+*/
+
+let url_cidades = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/estadoSelecionado/municipios"
+let url_estados = "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+
+// máscara do CPF
+var cpf = document.getElementById("CPF")
+
+cpf.addEventListener('keypress', () => {
+
+    let tamanho_cpf = cpf.value.length
+    if(tamanho_cpf == 3 || tamanho_cpf == 7){
+        cpf.value += '.'
+    }
+    else if(tamanho_cpf == 11){
+        cpf.value += '-'
+    }
+
+})
+//-------------------------------------------------------------------
+
+
+// máscara do celular
+var celular = document.getElementById("celular")
+
+celular.addEventListener('keypress', () => {
+
+    let tamanho_celular = celular.value.length
+    if(tamanho_celular == 0){
+        celular.value += '('
+    }
+    else if(tamanho_celular == 3){
+        celular.value += ')'
+    }
+    else if(tamanho_celular == 9){
+        celular.value += '-'
+    }
+
+})
+//-------------------------------------------------------------------
+
+
+const init = function(){
+    lista_estados()
+    lista_cidades()
+    document.getElementById('enviar').addEventListener('click', send);
+}
+
+
+//-------------------------------------------------------------------
+const send = function(ev){
+
+    let fails = validate();
+
+    if(fails.length === 0){
+        //good to go
+        //alert("O formulário foi enviado com sucesso.")
+        var dict = {} //dicionário que armazena as respostas
+
+
+        //armazenando cada elemento do form no dicionário
+        let nome = document.getElementById('nome')
+        dict['nome'] = nome.value
+        let sobrenome = document.getElementById('sobrenome')
+        dict['sobrenome'] = sobrenome.value
+        let email = document.getElementById('email')
+        dict['email'] = email.value
+        let cpf = document.getElementById('CPF')
+        dict['cpf'] = cpf.value
+        let masculino = document.getElementById('masculino')
+        dict['masculino'] = masculino.value
+        let feminino = document.getElementById('feminino')
+        dict['feminino'] = feminino.value
+        let nascimento = document.getElementById('nascimento')
+        dict['nascimento'] = nascimento.value
+        let endereço = document.getElementById('endereço')
+        dict['endereço'] = endereço.value
+        let celular = document.getElementById('celular')
+        dict['celular'] = celular.value
+        let profissão = document.getElementById('profissão')
+        dict['profissão'] = profissão.value
+        let informações_adicionais = document.getElementById('informações_adicionais')
+        dict['informações_adicionais'] = informações_adicionais.value
+        let estado = document.getElementById('estado')
+        dict['estado'] = estado.value
+        let cidade = document.getElementById('cidade')
+        dict['cidade'] = cidade.value
+        let senha = document.getElementById('senha')
+        dict['senha'] = senha.value
+        dict = [dict]
+        //salvando o dicionário
+        localStorage.setItem('formulário de '+nome.value+' '+sobrenome.value, JSON.stringify(dict))
+        document.getElementById('formulário').submit()
+
+        //document.getElementById('formulário').submit();
+    }else{
+        //ev.preventDefault(); 
+        //ev.stopPropagation();
+        //there are some errors to display
+        fails.forEach(function(obj){
+            let field = document.getElementById(obj.input);
+            field.setCustomValidity(obj.msg);
+            field.reportValidity();
+        })
+    }
+}
+//-------------------------------------------------------------------
+const validate = function(ev){
+
+    let failures = [];
+
+    //verificando se o cpf é válido
+    let cpf = document.getElementById("CPF");
+    if(!(/[0-9]{3}[\.][0-9]{3}[\.][0-9]{3}[\-][0-9]{2}/.test(cpf.value))){
+        if(cpf.value.length < 14){
+            failures.push({input:'CPF', msg:'O CPF deve ter exatamente 11 números.'})
+        }
+        else{
+            failures.push({input:'CPF', msg:'O CPF deve conter somente números.'})
+        }    
+    }
+
+    //verificando se o celular é válido
+    let celular = document.getElementById("celular");
+    if(!(/[\(][0-9]{2}[\)][0-9]{5}[\-][0-9]{4}/.test(celular.value))){
+        if(celular.value.length < 14){
+            failures.push({input:'celular', msg:'O celular deve ter exatamente 11 números.'})
+        }
+        else{
+            failures.push({input:'celular', msg:'O celular deve conter somente números.'})
+        }    
+    }
+
+    //verificando se ambas as senhas são iguais
+    let senha = document.getElementById('senha');
+    let confirmação_senha = document.getElementById('confirmação_senha');
+    if(!(senha.value === confirmação_senha.value)){
+        failures.push({input:'senha', msg:'Ambas as senhas devem ser iguais.'})
+    }
+
+    //verificando se a senha tem no mínimo 5 chars,
+    //no máximo 30 chars, no mínimo uma letra maiúscula,
+    //no mínimo 1 número, no mínimo um caractere especial
+    function senhaValida(senha){
+
+        var retorno = false;//variável que diz se a senha é válida ou n
+        var letrasMaiusculas = /[A-Z]/;//regex de letras maiúsculas
+        var numeros = /[0-9]/;//regex de números
+        var caracteresEspeciais = /[!|@|#|$|%|^|&|*|(|)|-|_]/;//regex de caracteres especiais
+        
+
+        if(senha.length > 30){//se a senha tem mais que 30 chars
+            return retorno;
+        }
+        if(senha.length < 5){//se a senha tem menos que 5 chars
+            return retorno;
+        }
+
+
+        var auxMaiuscula = 0;//variáveis auxiliares para contagem
+        var auxNumero = 0;
+        var auxEspecial = 0;
+
+
+        for(var i=0; i<senha.length; i++){//iterando caractere por caractere
+            if(letrasMaiusculas.test(senha[i]))//se é uma letra maiúscula
+                auxMaiuscula++;
+            else if(numeros.test(senha[i]))//se é um número
+                auxNumero++;
+            else if(caracteresEspeciais.test(senha[i]))//se é um caractere especial
+                auxEspecial++;
+        }
+
+
+        if (auxMaiuscula > 0){//tem pelo menos uma maiúscula
+            if (auxNumero > 0){//tem pelo menos um número
+                if (auxEspecial > 0){//tem pelo menos um caractere especial
+                    retorno = true;//senha é válida
+                }
+            }
+        }
+        return retorno;//se a senha n é válida
+    }
+
+    if(!senhaValida(senha.value)){//se a senha n é válida
+        failures.push({input:'senha', msg:'A senha deve ser preenchida da seguinte forma: no mínimo 5 dígitos e no máximo 30, com no mínimo uma letra maiúscula, um número e um caractere especial.'})
+    }
+
+    //verificando se a data é válida
+    function isValidDate(dateString){
+        // First check for the pattern
+        if(/\d{2}\/\d{2}\/\d{4}/.test(dateString))
+            return false;
+
+        // Parse the date parts to integers
+        var parts = dateString.split("-");
+        var day = parseInt(parts[2], 10);
+        var month = parseInt(parts[1], 10);
+        var year = parseInt(parts[0], 10);
+
+        // Check the ranges of month and year
+        if(year < 1000 || year > 3000 || month == 0 || month > 12)
+            return false;
+
+        var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+        // Adjust for leap years
+        if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+            monthLength[1] = 29;
+
+        // Check the range of the day
+        return day > 0 && day <= monthLength[month - 1];
+    };
+
+    let nascimento = document.getElementById('nascimento')
+
+    if(!isValidDate(nascimento.value)){
+        failures.push({input:'nascimento', msg:'Insira uma data válida.'})
+    }
+
+    //retornando as falhas
+    return failures;
+}
+
+//-------------------------------------------------------------------
+
+//em relação à listagem de estados e cidades
+
+const fazGet = function(url){//essa função pega a lista de estados/cidades
+    let request = new XMLHttpRequest()
+    request.open("GET", url, false)
+    request.send()
+    return request.responseText
+}
+
+const lista_estados = function(){//função que adiciona a lista de estados
+                                //ao select do form
+
+    var json = fazGet(url_estados) //conteúdo raw que vem do site do ibge
+    var data = JSON.parse(json) //transformando essa string raw em um json
+    //pq fica mais fácil de analisar com um json
+    var select_estado = document.getElementById('estado')
+    var lista = [] //lista que vai guarda os nomes dos estados
+    data.forEach(estado => {
+        lista.push(estado.nome+' ('+estado.sigla+')')
+    })
+    lista.sort() //ordenando a lista alfabeticamente
+    lista.forEach(estado =>{
+        var option = document.createElement('option')
+        option.textContent = estado
+        select_estado.append(option)
+    })
+
+}
+
+const lista_cidades = function(){//função que adiciona a lista de cidades
+                                //ao select do form conforme o estado selecionado
+
+    var estado_selecionado = document.getElementById('estado')
+    var UF = estado_selecionado.value.split('(')[1] //kkkkkkkkk
+    UF = UF.replace(')','') //aceitável
+    url = url_estados.split('estadoSelecionado') //aceitável tbm
+    url = url[0]+'/'+UF+'/municipios'
+    var json = fazGet(url) //conteúdo raw que vem do site do ibge
+    var data = JSON.parse(json) //transformando essa string raw em um json
+    //pq fica mais fácil de analisar com um json
+    var select_cidade = document.getElementById('cidade')
+
+    function remove_options(){//tem que limpar os options do select ;-;
+                            //para toda vez que há alteração no estado selecionado
+        document.getElementById("cidade").innerHTML = "";   
+    }
+    remove_options()
+
+    var lista = [] //lista que vai guarda os nomes das cidades
+    data.forEach(cidade => {
+        lista.push(cidade.nome)
+    })
+    lista.sort() //ordenando a lista alfabeticamente
+    lista.forEach(cidade =>{
+    var option = document.createElement('option')
+    option.textContent = cidade
+    select_cidade.append(option)
+    })
+
+}
+
+document.getElementById("estado").addEventListener('change', lista_cidades);
+//note que a alteração da lista das cidades só ocorre mediante alguma mudança
+//no estado selecionado
+
+//-------------------------------------------------------------------
+document.addEventListener('DOMContentLoaded', init);
+//em relação aos erros de inserção pelo usuário
